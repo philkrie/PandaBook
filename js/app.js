1c2f0989a856ae467f055d47b98d0fdab4983f67
Phillip Kriegel <philkrie@gmail.com>
2015-10-10 13:09:26 -0700

    Initial HTML, CSS, and JS. Utilizes Bootstrap and JQuery

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	
+	
+	
+	$("input[name=firstname]").val("Phillip");
+	$("input[name=lastname]").val("Kriegel");
+	$("input[name=city]").val("Beaverton");
+	$("input[name=state]").val("OR");
+	$("input[name=zip]").val("97007");
+	$("input[name=phone]").val("5038404219");
+	$("input[name=email]").val("philkrie@gmail.com");
+
+
+});
