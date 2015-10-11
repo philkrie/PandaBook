c292b09600e69333657d667253928376f63848e7
windowpain <windowpain@users.noreply.github.com>
2015-10-10 19:11:33 -0700

    Fixed and added access documentation to getlist.php
    It was not clear how the client page would access the returned data from
    the response text. Also, there was a quotation error in the creation of
    the custom attribute data-personid.

--- php/php_stubs/getlist.php
+++ php/php_stubs/getlist.php
@@ -9,6 +9,10 @@
      * Desc.:  Returns HTML table of Firstname, Lastname pairs for all entries
      *         in the address book database table. For this stub, the database
      *         is not accessed; returned data is hard-coded.
+     *
+     * How to access returned data: The returned table has id='entryListTable'
+     *     and each row (except header row) has the custom attribute
+     *     data-personid indicating the id of the entry.
      ==========================================================================
 -->
 
@@ -50,7 +54,7 @@ mysql_connect('ix-trusty:3022','xunl','tbc123bl') or die("Could not connect: " .
 */
 
 // Table header row.
-echo "<table>
+echo "<table id='entryListTable'>
 <tr>
 <!-- <th>Person ID</th> -->
 <th>Firstname</th>
@@ -69,7 +73,7 @@ $entryList = array(
 // Table data rows.
 foreach ($entryList as $row) {
     // Thanks to HTML 5, can define data-personid and access from Javascript. 
-    echo "<tr data-personid=" . ($row['id']) . ">";
+    echo "<tr data-personid='" . ($row['id']) . "''>";
     echo "<td>" . $row['first_name'] . "</td>";
     echo "<td>" . $row['last_name'] . "</td>";
     echo "</tr>";
